262143895ff0461a610224a74eb8e413ed457d10
JiSSung_E <hyt970@gmail.com>
2023-02-27 14:05:55 +0900

    annotation

--- sapp/api/sudokus.py
+++ sapp/api/sudokus.py
@@ -1,20 +1,27 @@
 import random
 
 class Sudoku():  
-
     def __init__(self):
-        self.SIZE = 9
+        # 보드 크기
+        self.SIZE = 9 
         self.origin_board = [[0 for j in range(0,self.SIZE)] for i in range(0,self.SIZE)]
+        # 스도쿠 2차원 board
         self.board = [[0 for j in range(0,self.SIZE)] for i in range(0,self.SIZE)]
+        # 어떤 숫자들이 채워졌는지를 체크하는 row, col 2차원 리스트
         self.row = [[0 for j in range(0,self.SIZE+1)] for i in range(0,self.SIZE+1)]
         self.col = [[0 for j in range(0,self.SIZE+1)] for i in range(0,self.SIZE+1)]
+        # 3 * 3 사각형에 어떤 숫자들이 채워졌는지를 체크하는 2차원 리스트 diag
         self.diag = [[0 for j in range(0,self.SIZE+1)] for i in range(0,self.SIZE+1)]
+        # DFS 기반의 백트래킹을 이용하므로 단 1개의 스도쿠 퍼즐만 생성하고 종료하기 위한
+        # 변수 terminate_flag
         self.terminate_flag = False
+        # 클래스 멤버를 초기화 해주는 함수
         self.__board_init()
 
+    # 클래스 멤버 초기화
     def __board_init(self):
+        # 1번째, 4번째, 9번째 3*3 사각형을 미리 채워주는 역할
         seq_diag = [0,4,8]
-
         for offset in range(0,9,3):
             seq = [i for i in range(1,self.SIZE+1)]
             random.shuffle(seq)
@@ -35,6 +42,7 @@ class Sudoku():
         self.terminate_flag = False
 
 
+    # 백트레킹을 이용하여, 미리 채워놓은 부분을 제외하고, 나머지 부분을 채워넣는 함수
     def __make_sudoku(self, k):
 
         if self.terminate_flag == True:
@@ -64,6 +72,7 @@ class Sudoku():
                 self.row[i][num],self.col[j][num],self.diag[d][num] = 0,0,0
                 self.origin_board[i][j] = 0
 
+    # 스도쿠가 정답인지 뿐만 아니라 잘못된 데이터가 들어갔는지도 같이 체크한다.
     def __sudoku_check(self, puzzle):
         if type(puzzle) != type([]) or len(puzzle) == 0:
             return False
